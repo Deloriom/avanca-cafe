6cf3e2d44ed21e3e821c6ca70d0b589c6c138d69
hvilela <henriquevilela@rubeus.com.br>
2020-10-19 20:29:33 -0300

    salva os dados

--- back/database/migrations/2020_06_07_233113_create_database.php
+++ back/database/migrations/2020_06_07_233113_create_database.php
@@ -18,6 +18,7 @@ class CreateDatabase extends Migration
             $table->id();
             $table->string('nome', 100);
             $table->string('codigo', 32);
+            $table->softDeletes();
             $table->timestamps();
         });
 
@@ -26,6 +27,7 @@ class CreateDatabase extends Migration
             $table->string('nome', 100);
             $table->unsignedBigInteger('proprietario_id');
             $table->foreign('proprietario_id')->references('id')->on('proprietario');
+            $table->softDeletes();
             $table->timestamps();
         });
 
@@ -38,66 +40,69 @@ class CreateDatabase extends Migration
             $table->decimal('espacamento_plantas', 10,2);
             $table->decimal('previsao_colheitas_saca', 10,2)->default(null);
             $table->decimal('saturacao_por_bases', 10,2)->default(null);
-            $table->decimal('prnt', 10,2);
+            $table->decimal('prnt', 10,2); // O poder relativo de neutralização total do calcario (PRNT) geralmente 76 na região
             $table->decimal('profundidade_corrigida', 10,2)->default(null);
             $table->decimal('superficie_cobertura', 10,2)->default(null);
 
             $table->unsignedBigInteger('propriedade_id');
             $table->foreign('propriedade_id')->references('id')->on('propriedade');
+            $table->softDeletes();
             $table->timestamps();
         });
 
         Schema::create('analise_solo', function (Blueprint $table) {
             $table->id();
-            $table->decimal('ph', 10, 2);
-            $table->decimal('p', 10, 2);
-            $table->decimal('ca', 10, 2);
-            $table->decimal('mg', 10, 2);
-            $table->decimal('k', 10, 2);
-            $table->decimal('so', 10, 2)->default(null);
-            $table->decimal('b', 10, 2)->default(null);
-            $table->decimal('zn', 10, 2)->default(null);
-            $table->decimal('cu', 10, 2)->default(null);
-            $table->decimal('mn', 10, 2)->default(null);
-            $table->decimal('fe', 10, 2)->default(null);
-            $table->decimal('ai', 10, 2)->default(null);
-            $table->decimal('t_mi', 10, 2)->default(null);
-            $table->decimal('h_ai', 10, 2)->default(null);
-            $table->decimal('m', 10, 2)->default(null);
-            $table->decimal('sb', 10, 2)->default(null);
+            $table->decimal('ph', 10, 2); // ph do solo
+            $table->decimal('p', 10, 2); // fosoforo
+            $table->decimal('ca', 10, 2); // calcio
+            $table->decimal('mg', 10, 2); // magnesio
+            $table->decimal('k', 10, 2); // potassio
+            $table->decimal('so', 10, 2)->default(null); // 
+            $table->decimal('b', 10, 2)->default(null); // boro
+            $table->decimal('zn', 10, 2)->default(null); // zinco
+            $table->decimal('cu', 10, 2)->default(null); // cobre
+            $table->decimal('mn', 10, 2)->default(null); // manganes
+            $table->decimal('fe', 10, 2)->default(null); // ferro
+            $table->decimal('ai', 10, 2)->default(null); // aluminio
+            $table->decimal('t_mi', 10, 2)->default(null); // 
+            $table->decimal('h_ai', 10, 2)->default(null); //
+            $table->decimal('m', 10, 2)->default(null); //
+            $table->decimal('sb', 10, 2)->default(null); // soma de bases
             $table->decimal('t_ma', 10, 2)->default(null);
-            $table->decimal('v', 10, 2)->default(null);
-            $table->decimal('ca_ctc', 10, 2)->default(null);
-            $table->decimal('mg_ctc', 10, 2)->default(null);
-            $table->decimal('k_ctc', 10, 2)->default(null);
-            $table->decimal('ca_mg', 10, 2)->default(null);
+            $table->decimal('v', 10, 2)->default(null); // saturação de bases
+            $table->decimal('ca_ctc', 10, 2)->default(null); // percentual de calcio
+            $table->decimal('mg_ctc', 10, 2)->default(null); // percentual de magnesio
+            $table->decimal('k_ctc', 10, 2)->default(null);  // percentual de potassio
+            $table->decimal('ca_mg', 10, 2)->default(null); // relação calcio magnesio
             $table->decimal('materia_organica', 10, 2)->default(null);
             $table->decimal('teor_argila', 10, 2)->default(null);
 
             $table->unsignedBigInteger('talhao_id');
             $table->foreign('talhao_id')->references('id')->on('talhao');
+            $table->softDeletes();
             $table->timestamps();
         });
 
         Schema::create('recomendacao', function (Blueprint $table) {
             $table->id();
-            $table->date('data_recomendacao');
+            $table->date('data_recomendacao'); 
             $table->char('safra_ano', 9);
-            $table->decimal('quantidade_calcario_ha', 10, 2);
+            $table->decimal('quantidade_calcario_ha', 10, 2); // quantidade_calcario por hectare
             $table->decimal('gramas_metro_linear', 10, 2);
             $table->char('tipo_calcario', 20);
-            $table->decimal('prnt', 10, 2);
+            $table->decimal('prnt', 10, 2); // O poder relativo de neutralização total do calcario (PRNT) geralmente 76 na região
             $table->decimal('quantidade_calcario_talhao', 10, 2);
             $table->decimal('nitrogenio', 10, 2);
             $table->decimal('fosforo', 10, 2);
             $table->decimal('potassio', 10, 2);
             $table->char('fonte', 8);
-            $table->decimal('kg_ha', 10, 2);
+            $table->decimal('kg_ha', 10, 2); // kilos por hectares
             $table->decimal('sc_talhao', 10, 2);
             $table->decimal('gramas_planta', 10, 2);
             $table->decimal('gramas_metro', 10, 2);
             $table->unsignedBigInteger('analise_solo_id');
             $table->foreign('analise_solo_id')->references('id')->on('analise_solo');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
