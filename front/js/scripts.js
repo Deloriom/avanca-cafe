7741209284814abfd0c8dd8c9f27f644c5f453c2
Renan Andrade <filgueiras.rfa@gmail.com>
2020-10-31 19:21:16 -0300

    adicionada validação se campo está vazio, adicionado também alteração do texto da esquerda de acordo com mudança do select do tipo de calculo, adicionado controle de aparição da seção de resultado caso a api retorne true

--- front/js/scripts.js
+++ front/js/scripts.js
@@ -15,6 +15,7 @@ let loading = '';
 let divSatBase = '';
 let divTeorAl = '';
 let divCalMag = '';
+let divResult = '';
 
 window.addEventListener('load', () => {
     var elems = document.querySelectorAll('select');
@@ -32,6 +33,7 @@ function loadFields() {
     saturacaoIdeal = document.querySelector('#saturacao-ideal');
     saturacaoSolo = document.querySelector('#saturacao-solo');
     ctc = document.querySelector('#ctc');
+    magnesio = document.querySelector('#magnesio');
     calcio = document.querySelector('#calcio');
     aluminio = document.querySelector('#aluminio');
     teorArgila = document.querySelector('#teor-argila');
@@ -41,29 +43,52 @@ function loadFields() {
     divSatBase = document.querySelector('#div-sat-base');
     divTeorAl = document.querySelector('#div-teor-al');
     divCalMag = document.querySelector('#div-cal-mag');
+    divResult = document.querySelector('#div-result');
 }
 
 function controleFields(event) {
     const opt = event.target.value;
+    removeValidates([proprietario, talhao, saturacaoSolo, saturacaoIdeal, ctc, magnesio, calcio, aluminio, teorArgila, teorMaximo]);
 
-    console.log(opt)
     if (opt == 1) {
         teorArgila.parentNode.classList.add('none');
         teorMaximo.parentNode.classList.add('none');
+        calcio.parentNode.classList.add('none');
+        aluminio.parentNode.classList.add('none');
+        magnesio.parentNode.classList.add('none');
+
+        saturacaoIdeal.parentNode.classList.remove('none');
+        saturacaoSolo.parentNode.classList.remove('none');
+
         divSatBase.classList.remove('none');
         divTeorAl.classList.add('none');
         divCalMag.classList.add('none');
+
     }
     if (opt == 2) {
+        saturacaoIdeal.parentNode.classList.add('none');
+        saturacaoSolo.parentNode.classList.add('none');
+
         teorArgila.parentNode.classList.remove('none');
         teorMaximo.parentNode.classList.remove('none');
+        calcio.parentNode.classList.remove('none');
+        aluminio.parentNode.classList.remove('none');
+        magnesio.parentNode.classList.remove('none');
+
         divSatBase.classList.add('none');
         divTeorAl.classList.remove('none');
         divCalMag.classList.add('none');
     }
     if (opt == 3) {
+        saturacaoIdeal.parentNode.classList.add('none');
+        saturacaoSolo.parentNode.classList.add('none');
         teorArgila.parentNode.classList.add('none');
         teorMaximo.parentNode.classList.add('none');
+        aluminio.parentNode.classList.add('none');
+
+        calcio.parentNode.classList.remove('none');
+        magnesio.parentNode.classList.remove('none');
+
         divSatBase.classList.add('none');
         divTeorAl.classList.add('none');
         divCalMag.classList.remove('none');
@@ -71,10 +96,25 @@ function controleFields(event) {
 }
 
 
-
-
 function realizaCalculo(event) {
     event.preventDefault();
+    if (tipoCalculo.value == 1) {
+        if (validateFields([proprietario, talhao, saturacaoSolo, saturacaoIdeal, ctc])) {
+            return;
+        }
+    }
+
+    if (tipoCalculo.value == 2) {
+        if (validateFields([proprietario, talhao, ctc, magnesio, calcio, aluminio, teorArgila, teorMaximo])) {
+            return;
+        }
+    }
+
+    if (tipoCalculo.value == 3) {
+        if (validateFields([proprietario, talhao, ctc, magnesio, calcio])) {
+            return;
+        }
+    }
 
     const dados = {
         "tipo_calculo": tipoCalculo.value,
@@ -94,11 +134,28 @@ function realizaCalculo(event) {
     btnConfirma.disabled = true;
     setTimeout(() => {
         reqApi(dados)
-        console.log(dados)
     }, 1000);
-    reqApi(dados)
 }
 
+function removeValidates(fields) {
+    fields.forEach(field => {
+        field.classList.remove("invalid");
+    })
+}
+
+function validateFields(fields) {
+    let erro = false;
+    fields.forEach(field => {
+        if (field.value == '') {
+            erro = true;
+            field.classList.add("invalid");
+        }
+    })
+
+    if (erro) {
+        return true;
+    }
+}
 
 function reqApi(dados) {
     const api = new XMLHttpRequest();
@@ -110,9 +167,18 @@ function reqApi(dados) {
             var data = api.responseText;
             loading.classList.add('none')
             btnConfirma.disabled = false;
+            reqTrue()
         } else {
             loading.classList.add('none')
             btnConfirma.disabled = false;
         }
     }
 }
+
+
+function reqTrue() {
+    divResult.classList.remove('none');
+    window.scrollTo({ top: divResult.getBoundingClientRect().top, behavior: 'smooth' })
+    console.log(divResult.getBoundingClientRect().top)
+}
+
